ff81c25b4592d38bc1cbd5fc687e119cee3b2cd2
“Veeltu” <pawelandrys@gmail.com>
2022-10-09 06:34:41 +0200

    commit

--- JS advance/9 JS zadania OOP cz.2/zadanie2.js
+++ JS advance/9 JS zadania OOP cz.2/zadanie2.js
@@ -14,15 +14,18 @@
 // ver. 1 - simple
 
 
+// random to volume for class WaterVessel
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
 class WaterVessel {
     id;
-    volume;
+    volume = getRandom(50,200); //random function
     Water_volume;
     constructor(id,volume,Water_volume){
     this.id = id;
-    this.volume = volume;
     this.Water_volume = Water_volume;
-
     }
     get id(){
         return this.id;
@@ -34,23 +37,24 @@ class WaterVessel {
         return this.Water_volume;
     }
 }
-
-const Vessel_1 = new WaterVessel('1', 50, 0);
-const Vessel_2 = new WaterVessel('2', 400, 0);
-const Vessel_3 = new WaterVessel('3', 140, 0);
-const Vessel_4 = new WaterVessel('4', 400, 0);
-const Vessel_5 = new WaterVessel('5', 400, 0);
-
+// add 5 objects to class
+const Vessel_1 = new WaterVessel('1', '${volume}', 0);
+const Vessel_2 = new WaterVessel('2', '${volume}', 0);
+const Vessel_3 = new WaterVessel('3', '${volume}', 0);
+const Vessel_4 = new WaterVessel('4', '${volume}', 0);
+const Vessel_5 = new WaterVessel('5', '${volume}', 0);
+// make array from objects to literate
 const list = [Vessel_1, Vessel_2, Vessel_3, Vessel_4, Vessel_5];
-
-let Water_canister = 600;
-
-
-var sorted_list = list.sort((a, b) => b.volume-a.volume);
+// variable, water to split
+let Water_canister = 1000;
+// sort objects from class, by biggest volume, and take biggest one, for loop
+const sorted_list = list.sort((a, b) => b.volume-a.volume);
 let biggestVessel = sorted_list[0];
 
 
 // while działa dopoki co najmniej jeden warunek jest true ( ||), po ! stop jeśli conajmniej jeden warunek jest true;
+
+// core loop
 while(!(biggestVessel.Water_volume == biggestVessel.volume || Water_canister<=0)){
     list.forEach(eWaterVessel => {
        if(eWaterVessel.Water_volume<eWaterVessel.volume && Water_canister>0){
@@ -59,11 +63,22 @@ while(!(biggestVessel.Water_volume == biggestVessel.volume || Water_canister<=0)
         Water_canister -= 1;
     }});
 }
+
+
 console.log(list);
+
+function howMuchLeftWater(){
+    if(Water_canister<=0){
+        console.log(`Water_canister is empty`);
+    } else{
+        console.log(`Left ${Water_canister} water in Water_canister`);
+    }
+}
+howMuchLeftWater();
 
 let sum = 0;    
 list.forEach(e => sum+= e.Water_volume);
-console.log(sum);
+console.log(`We use ${sum} water from Water_canister`);
 
 //git test add
 
