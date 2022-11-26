d21f69036dc19789183c7c3babde98d11c9e232a
“Veeltu” <pawelandrys@gmail.com>
2022-11-26 14:41:13 +0100

    Co-authored-by: jszachew <jszachew@users.noreply.github.com>

--- react/1react/zadanie3/zadanie3/src/components/grid/grid.jsx
+++ react/1react/zadanie3/zadanie3/src/components/grid/grid.jsx
@@ -18,28 +18,21 @@ function Grid() {
       // style={{ backgroundImage: `url(${backgroundImg})` }}
     >
       <div className="grid-container">
-        <div className="container container-1">
-          <Card
-            name={names[0]}
-            surname={surnames[0]}
-            avatar={avatarData[0]}
-            backgroundImg={backgroundImg[0]}
-            bronze={bronze[0]}
-            silver={silver[0]}
-            gold={gold[0]}
-          />
-        </div>
-        <div className="container container-2">
-          <Card
-            name={names[1]}
-            surname={surnames[1]}
-            avatar={avatarData[1]}
-            backgroundImg={backgroundImg[0]}
-            bronze={bronze[1]}
-            silver={silver[1]}
-            gold={gold[1]}
-          />
-        </div>
+        {
+          Data.map(person => (
+            <div className="container">
+              <Card
+                name={person.name}
+                surname={person.surname}
+                avatar={person.avatar}
+                backgroundImg={person.backgroundImg}
+                bronze={person.bronze}
+                silver={person.silver}
+                gold={person.gold}
+            />
+            </div>
+          ))
+        }
       </div>
       {/* <Card
         name={names[0]}
