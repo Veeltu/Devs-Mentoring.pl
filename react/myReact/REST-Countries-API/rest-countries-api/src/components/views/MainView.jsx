a9f4379ce2585f13daf34f321d7e873a691ceb9f
“Veeltu” <pawelandrys@gmail.com>
2023-01-08 21:59:59 +0100

    commit

--- react/myReact/REST-Countries-API/rest-countries-api/src/components/views/MainView.jsx
+++ react/myReact/REST-Countries-API/rest-countries-api/src/components/views/MainView.jsx
@@ -1,5 +1,6 @@
 import Countries from "../features/countries/countries";
 import { countriesData } from "../../data/countries";
+import { useState, useEffect } from "react";
 // useState, useEffect
 const MainView = () => {
   /*
@@ -7,15 +8,23 @@ const MainView = () => {
     2. Wartosci z selecta przechowywac w useState
     3. filter uruchamiany jest tylko wtedy, gdy zmienia sie wybrany kontynent (useEffect)
   */
-  const continentAfrica = countriesData.filter((e) => e.region === "Africa"); // TODO: dodać hooki 
+ 
+ const [continent, setContinent] = useState();
+ const [continentFilter, setContinentFilter] = useState();
 
+ useEffect(() => {
+    const continentFIlter = countriesData.filter((e) => e.region === continent); // TODO: dodać hooki 
+    setContinentFilter(continentFIlter)
+  }) //when
+
+// onClick "Africa" set continent ==="Africa" => filter and set props
   
 
-  console.log(continentAfrica);
 
   return (
     <div className="body">
-      <Countries data={continentAfrica} />
+      <button onClick={() => setContinent("Africa")}><code>set Africa</code></button>
+      <Countries data={continentFilter} />
     </div>
   );
 };
