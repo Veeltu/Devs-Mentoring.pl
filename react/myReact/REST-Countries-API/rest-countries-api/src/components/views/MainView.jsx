d121e3d2e64db917b3f65c99435b86cb9fa9a966
“Veeltu” <pawelandrys@gmail.com>
2023-01-11 21:01:10 +0100

    commit

--- react/myReact/REST-Countries-API/rest-countries-api/src/components/views/MainView.jsx
+++ react/myReact/REST-Countries-API/rest-countries-api/src/components/views/MainView.jsx
@@ -1,33 +1,50 @@
 import Countries from "../features/countries/countries";
 import { countriesData } from "../../data/countries";
 import { useState, useEffect } from "react";
-// useState, useEffect
+
 const MainView = () => {
   /*
     1. Dodać select po kontynencie
     2. Wartosci z selecta przechowywac w useState
     3. filter uruchamiany jest tylko wtedy, gdy zmienia sie wybrany kontynent (useEffect)
-  */
- 
- const [continent, setContinent] = useState();
- const [continentFilter, setContinentFilter] = useState();
+    TODO: how to filter ALL ?
+    */
 
- useEffect(() => {
-    const continentFIlter = countriesData.filter((e) => e.region === continent); // TODO: dodać hooki 
-    setContinentFilter(continentFIlter)
-  }) //when
+  const [continent, setContinent] = useState("Asia");
+  const [contFilter, setContFilter] = useState([]); //important : this have to be [], for map function
 
-// onClick "Africa" set continent ==="Africa" => filter and set props
-  
+  useEffect(() => {
+    const b = countriesData.filter((e) => e.region === continent);
+    setContFilter(b)
+    // console.log(b)
+  }, [continent]);
 
+  // console.log(contFilter) // works
 
   return (
     <div className="body">
-      <button onClick={() => setContinent("Africa")}><code>set Africa</code></button>
-      <Countries data={continentFilter} />
+      <div className="container m-5">
+        <select className="custom-select">
+          <option onClick={() => setContinent("Asia")} value="Asia">
+            Asia
+          </option>
+          <option onClick={() => setContinent("Americas")} value="Americas">
+            Americas
+          </option>
+          <option onClick={() => setContinent("Africa")} value="Africa">
+            Africa
+          </option>
+          <option onClick={() => setContinent("Europe")} value="Europe">
+            Europe
+          </option>
+          {/* <option onClick={() => setContinent()} value="All">
+            All // TODO : how to get filter with all
+          </option> */}
+        </select>
+      </div>
+      <Countries data={contFilter} />
     </div>
   );
 };
 
 export default MainView;
-
