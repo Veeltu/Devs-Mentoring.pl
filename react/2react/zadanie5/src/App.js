659fdb047cb11681a29da062a40f2ac481b3d321
“Veeltu” <pawelandrys@gmail.com>
2023-01-15 21:50:55 +0100

    commit

---
+++ react/2react/zadanie5/src/App.js
@@ -0,0 +1,39 @@
+
+// Zad 5 
+// Stwórz custom hooka useClickOutside, 
+// który jako parametr przyjmie wartość ref z hooka useRef oraz funkcję callback, 
+// który będzie służył do nasłuchiwania kliknięć na zewnętrzne elementy. 
+
+// Jako funkcję callback możesz przekazać funkcję zamykającą modal, 
+// do którego przypięty jest ref.
+
+// user story : click modal => open window, click outside window => close window
+
+
+
+import { useState } from 'react';
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ModalBootstrap from './components/Modal/ModalBootstrap';
+
+
+function App() {
+  // hide/show MODAL
+  const [showModal, setShowModal] = useState(false)
+  const openModal = () => {
+    setShowModal(prev => !prev)
+  }
+
+
+  return (
+    <div className='container'>
+        <Button className="button" size="lg" onClick={openModal}>click me</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal}/>
+        {/* <ModalBootstrap/>  */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react/2react/zadanie5/src/components/Modal/Modal.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import './Modal.css'
+
+function Modal({ showModal, setShowModal }) {
+
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  return (
+    <>
+
+      {showModal ? (
+        <div className="background" onClick={closeModal} ref={modalRef}>
+            <div className="modalWrapper">
+              <h1>Are you ready?</h1>
+              <p>Get exclusive access to our next launch.</p>
+              <button>Join Now</button>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+export default Modal;
