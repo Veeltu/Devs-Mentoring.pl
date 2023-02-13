d936ee27ec2f403a97a216ba29f00876f6809ecc
Veeltu <pawelandrys@gmail.com>
2023-02-13 20:58:03 +0100

    commit

--- react/3react/appzadanie1/src/Orders/checkbox.js
+++ react/3react/appzadanie1/src/Orders/checkbox.js
@@ -4,25 +4,25 @@ import axios from "axios";
 import { useState } from "react";
 const url = "http://localhost:3000/order/";
 
-
 export default function ControlledCheckbox({ refresh, row }) {
+  const [values, setValues] = useState({
+    id: row.id,
+    order: row.order,
+    isReady: row.isReady,
+  });
 
-  const [values, setValues ] = useState({
-                                              id: row.id,
-                                              order: row.order,
-                                              isReady: row.isReady,
-                                          })
-  
   const handleChange = (event) => {
-      setValues({...values, isReady: event.target.checked});
-      const handlePut = async () => {
-        try {
-          const resp = await axios.put(url + row.id, {...values, isReady:event.target.checked});
-          console.log(resp)
-          console.log(resp.data)
-          refresh();
-        } catch (error) {}
-        
+    setValues({ ...values, isReady: event.target.checked });
+    const handlePut = async () => {
+      try {
+        const resp = await axios.put(url + row.id, {
+          ...values,
+          isReady: event.target.checked,
+        });
+        console.log(resp);
+        console.log(resp.data);
+        refresh();
+      } catch (error) {}
     };
     handlePut();
   };
