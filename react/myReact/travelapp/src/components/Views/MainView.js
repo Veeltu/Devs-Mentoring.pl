81c523ba5e7c819b2bec1509cf777778abc3e140
Veeltu <pawelandrys@gmail.com>
2023-02-17 15:11:41 +0100

    commit

--- react/myReact/travelapp/src/components/Views/MainView.js
+++ react/myReact/travelapp/src/components/Views/MainView.js
@@ -4,8 +4,8 @@ import UpperTab from "../features/UpperTab/UpperTab";
 function MainView() {
   return (
     <div className="">
-      <UpperTab />
-      <CountriesGrid/>
+      <UpperTab /> // dark/light mode
+      <CountriesGrid/> // with filtrs
     </div>
   );
 }
